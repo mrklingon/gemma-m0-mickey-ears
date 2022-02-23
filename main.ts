40b75b77feb9f64c72c9fae0cc9e3e56c85764c6
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-02-23 09:34:55 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,28 @@
 input.touchD1.onEvent(ButtonEvent.Up, function () {
     strip.stopAllAnimations()
-    strip.showAnimation(light.sparkleAnimation, 3000)
+    magic(spell)
+    pixel.setColor(pixel.rgb(randint(10, 120), randint(10, 120), randint(10, 120)))
+    spell += 1
+    if (spell > 3) {
+        spell = 0
+    }
 })
+function magic (num: number) {
+    if (num == 0) {
+        strip.showAnimation(light.sparkleAnimation, 3000)
+    }
+    if (num == 1) {
+        strip.showAnimation(light.rainbowAnimation, 4000)
+    }
+    if (num == 2) {
+        strip.showAnimation(light.cometAnimation, 4000)
+    }
+    if (num == 32) {
+        strip.showAnimation(light.runningLightsAnimation, 3000)
+    }
+}
+let spell = 0
 let strip: light.NeoPixelStrip = null
 strip = light.createStrip(pins.D0, 30)
 strip.setBrightness(30)
-forever(function () {
-    strip.showAnimation(light.colorWipeAnimation, 3000)
-    strip.showAnimation(light.rainbowAnimation, 4000)
-    strip.showAnimation(light.cometAnimation, 4000)
-    strip.showAnimation(light.runningLightsAnimation, 3000)
-})
+spell = 0
